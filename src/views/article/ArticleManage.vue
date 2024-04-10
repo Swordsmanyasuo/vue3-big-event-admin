8adab0fa4b166528c4ada1f6d2f24d9df9e45df3
Swordsmanyasuo <123147705+Swordsmanyasuo@users.noreply.github.com>
2024-04-10 12:00:59 +0800

    文章分类

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>文章管理</div>
+  <page-container title="文章管理">
+    <template #extra>
+      <el-button type="primary"> 添加文章 </el-button>
+    </template>
+
+    文章管理
+  </page-container>
 </template>
 
 <script setup></script>
